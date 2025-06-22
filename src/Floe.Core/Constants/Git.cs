fb25ffa2114fb0ef20b9b40b173df784eea5bd6b
Christian <christian@arensbak.dev>
2025-06-22 22:07:19 +0200

    feat: Add more git constants.

--- src/Floe.Core/Constants/Git.cs
+++ src/Floe.Core/Constants/Git.cs
@@ -3,15 +3,19 @@ namespace Floe.Core.Constants;
 public static class Git
 {
     public const string BaseName = "git";
+    public const string Head = "HEAD";
+    public const string Merge = "merge";
+    public const string Branch = "branch";
 
-    public static class Merge
+    public static class MergeFlags
     {
-        public const string BaseName = "merge";
+        public const string NoFastForward = "--no-ff";
+        public const string Message = "--message";
+    }
 
-        public static class Flags
-        {
-            public const string NoFastForward = "--no-ff";
-            public const string Message = "--message";
-        }
+    public static class BranchFlags
+    {
+        public const string FormatRefnameShort = "--format=\"%(refname:short)\"";
+        public const string ShowCurrent = "--show-current";
     }
 }
