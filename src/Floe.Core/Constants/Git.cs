dd5289ed5da1d8707187ce146d92e106dba89c06
Christian <christian@arensbak.dev>
2025-06-25 21:59:12 +0200

    feat: Add a bunch of git related constants.

--- src/Floe.Core/Constants/Git.cs
+++ src/Floe.Core/Constants/Git.cs
@@ -4,8 +4,27 @@ public static class Git
 {
     public const string BaseName = "git";
     public const string Head = "HEAD";
+    public const string Origin = "origin";
+    public const string Push = "push";
+    public const string Pull = "pull";
     public const string Merge = "merge";
     public const string Branch = "branch";
+    public const string Fetch = "fetch";
+    public const string Tag = "tag";
+
+    public static class DefaultBranchNames
+    {
+        public const string Master = "master";
+        public const string Dev = "dev";
+        public const string Fix = "fix";
+        public const string Feature = "feature";
+        public const string Release = "release";
+    }
+
+    public static class PushFlags
+    {
+        public const string SetUpstream = "--set-upstream";
+    }
 
     public static class MergeFlags
     {
@@ -17,5 +36,9 @@ public static class Git
     {
         public const string FormatRefnameShort = "--format=\"%(refname:short)\"";
         public const string ShowCurrent = "--show-current";
+    }
+    public static class FetchFlags
+    {
+        public const string All = "--all";
     }
 }
