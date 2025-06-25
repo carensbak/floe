6f092ce74ffa674c15d24989d3a9c136e273a44b
Christian <christian@arensbak.dev>
2025-06-25 22:01:32 +0200

    feat: Add helpers that can fetch all remotes, create tags and branches.

--- src/Floe.Core/Utils/GitUtils.cs
+++ src/Floe.Core/Utils/GitUtils.cs
@@ -5,25 +5,79 @@ namespace Floe.Core.Utils;
 
 public static class GitUtils
 {
-    public static List<string> GetRepoBranches()
+    public static void FetchAllRemotes()
+    {
+        using var process = StdProcess.CreateStandardProcess(Git.BaseName, $"{Git.Fetch} {Git.FetchFlags.All}");
+        process.Start();
+
+        process.WaitForExit();
+    }
+
+    public static async Task<List<string>> GetRepoBranchesAsync()
     {
         using var process = StdProcess.CreateStandardProcess(Git.BaseName, $"{Git.Branch} {Git.BranchFlags.FormatRefnameShort}");
         process.Start();
 
         var branches = process.StdOutToList();
-        process.WaitForExit();
+        await process.WaitForExitAsync();
 
         return branches;
     }
 
-    public static string GetCurrentBranch()
+    public static async Task<string> GetCurrentBranchAsync()
     {
         using var process = StdProcess.CreateStandardProcess(Git.BaseName, $"{Git.Branch} {Git.BranchFlags.ShowCurrent}");
         process.Start();
 
         var branch = process.GetStdOutFirstLine();
-        process.WaitForExit();
+        await process.WaitForExitAsync();
 
         return branch;
+    }
+
+    public static async Task<bool> CreateBranch(string branchName, bool? pushToRemote)
+    {
+        using var createBranchProcess = StdProcess.CreateStandardProcess(Git.BaseName, [Git.Branch, branchName]);
+        createBranchProcess.Start();
+        await createBranchProcess.WaitForExitAsync();
+
+        if (createBranchProcess.ExitCode != 0)
+            return false;
+
+        if (pushToRemote ?? true)
+        {
+            using var pushProcess = StdProcess.CreateStandardProcess(Git.BaseName, [
+                Git.Push, Git.Origin,
+                Git.PushFlags.SetUpstream, branchName]);
+            pushProcess.Start();
+            await pushProcess.WaitForExitAsync();
+
+            if (pushProcess.ExitCode != 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    public static async Task<bool> CreateTag(string tag, bool pushToRemote)
+    {
+        using var createTagProcess = StdProcess.CreateStandardProcess(Git.BaseName, [Git.Tag, tag]);
+        createTagProcess.Start();
+        await createTagProcess.WaitForExitAsync();
+
+        if (createTagProcess.ExitCode != 0)
+            return false;
+
+        if (pushToRemote)
+        {
+            using var pushProcess = StdProcess.CreateStandardProcess(Git.BaseName, [Git.Push, Git.Origin, tag]);
+            pushProcess.Start();
+            await pushProcess.WaitForExitAsync();
+
+            if (pushProcess.ExitCode != 0)
+                return false;
+        }
+
+        return true;
     }
 }
