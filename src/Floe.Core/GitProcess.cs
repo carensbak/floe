22ddb75bd2edf49b8e16fc2325b9f07583241897
Christian <christian@arensbak.dev>
2025-06-29 21:24:32 +0200

    refactor: Create a domain specific GitProcess object, rather  than a generic process.

---
+++ src/Floe.Core/GitProcess.cs
@@ -0,0 +1,44 @@
+using System.Diagnostics;
+using System.Text;
+
+namespace Floe.Core;
+
+public abstract class GitProcess
+{
+    protected StringBuilder ArgsBuilder { get; } = new();
+
+    public abstract Process Execute();
+    protected Process Execute(string command)
+    {
+        var process = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = Git.BaseName,
+                Arguments = string.Join(' ', command, ArgsBuilder.ToString()),
+                RedirectStandardError = true,
+                RedirectStandardOutput = true,
+            }
+        };
+
+        process.Start();
+
+        return process;
+    }
+
+    public abstract void ExecuteAndFinish();
+    protected void ExecuteAndFinish(string command)
+    {
+        using var process = Execute(command);
+
+        process.WaitForExit();
+    }
+
+    public abstract Task ExecuteAsync();
+    protected async Task ExecuteAsync(string command)
+    {
+        using var process = Execute(command);
+
+        await process.WaitForExitAsync();
+    }
+}
