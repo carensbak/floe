13d0d18b3b1ed866bd8005888d9261daec4b86c7
Arensbak <char@novax.dk>
2025-07-14 11:31:48 +0200

    feat: Add builder methods to set the upstream, and reference origin.

--- src/Floe.Core/Builders/PushBuilder.cs
+++ src/Floe.Core/Builders/PushBuilder.cs
@@ -12,6 +12,27 @@ public class PushBuilder : GitProcess
         ArgsBuilder.AppendArgument(refname);
     }
 
+    public PushBuilder Origin()
+    {
+        ArgsBuilder.AppendArgument(Git.Origin);
+
+        return this;
+    }
+
+    public PushBuilder SetOriginUpstream(string upstream)
+    {
+        SetUpstream($"{Git.Origin} {upstream}");
+
+        return this;
+    }
+
+    public PushBuilder SetUpstream(string upstream)
+    {
+        ArgsBuilder.AppendArgument($"{Git.PushFlags.SetUpstream} {upstream}");
+
+        return this;
+    }
+
     public override Process Execute() => base.Execute(Git.CommandNames.Push);
     public override void ExecuteAndFinish() => base.ExecuteAndFinish(Git.CommandNames.Push);
     public override Task ExecuteAsync() => base.ExecuteAsync(Git.CommandNames.Push);
