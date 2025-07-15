fd14716c837fdab522cff84e44c4b48f30319860
Arensbak <char@novax.dk>
2025-07-15 09:49:08 +0200

    style: Convert single lune functions to arrow functions.

--- src/Floe.Core/Extensions/StringExtensions.cs
+++ src/Floe.Core/Extensions/StringExtensions.cs
@@ -1,6 +1,6 @@
 using System.Text.RegularExpressions;
 
-using Floe.Core.Constants;
+using Floe.Core.Models;
 
 namespace Floe.Core.Extensions;
 
@@ -9,35 +9,12 @@ public static partial class StringExtensions
     [GeneratedRegex(@"\bv?(\d+)\.(\d+)\.(\d+)\b")] //See SYSLIB1045
     private static partial Regex SemverRegex();
 
-    public static bool IsMasterBranch(this string branch)
-    {
-        return branch.Contains(Git.BranchNames.Master);
-    }
-
-    public static bool IsDevBranch(this string branch)
-    {
-        return branch.Contains(Git.BranchNames.Dev);
-    }
-
-    public static bool IsFeatureBranch(this string branch)
-    {
-        return branch.Contains(Git.BranchNames.Feature);
-    }
-
-    public static bool IsFixBranch(this string branch)
-    {
-        return branch.Contains(Git.BranchNames.Fix);
-    }
-
-    public static bool IsReleaseBranch(this string branch)
-    {
-        return branch.Contains(Git.BranchNames.Release);
-    }
-
-    public static bool ContainsSemver(this string branch)
-    {
-        return SemverRegex().IsMatch(branch);
-    }
+    public static bool IsMasterBranch(this string branch) => branch.Contains(Git.BranchNames.Master);
+    public static bool IsDevBranch(this string branch) => branch.Contains(Git.BranchNames.Dev);
+    public static bool IsFeatureBranch(this string branch) => branch.Contains(Git.BranchNames.Feature);
+    public static bool IsFixBranch(this string branch) => branch.Contains(Git.BranchNames.Fix);
+    public static bool IsReleaseBranch(this string branch) => branch.Contains(Git.BranchNames.Release);
+    public static bool ContainsSemver(this string branch) => SemverRegex().IsMatch(branch);
 
     public static string? TryGetSemver(this string branch)
     {
