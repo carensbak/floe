08163ceb91bc88b1972a2e136f3052d585cae55c
Arensbak <char@novax.dk>
2025-07-15 14:42:53 +0200

    refactor: Require args to be passed rather than calling the ArgsBuilder.ToString().

--- src/Floe.Core/Models/GitProcess.cs
+++ src/Floe.Core/Models/GitProcess.cs
@@ -8,14 +8,14 @@ public abstract class GitProcess : IGitProcess
     protected StringBuilder ArgsBuilder { get; } = new();
 
     public abstract Process Execute();
-    protected Process Execute(string command)
+    protected Process Execute(string command, string args)
     {
         var process = new Process
         {
             StartInfo = new ProcessStartInfo
             {
                 FileName = Git.BaseName,
-                Arguments = string.Join(' ', command, ArgsBuilder.ToString()),
+                Arguments = string.Join(' ', command, args),
                 RedirectStandardError = true,
                 RedirectStandardOutput = true,
             }
@@ -27,22 +27,26 @@ public abstract class GitProcess : IGitProcess
     }
 
     public abstract void ExecuteAndFinish();
-    protected void ExecuteAndFinish(string command)
+    protected void ExecuteAndFinish(string command, string args)
     {
-        using var process = Execute(command);
+        using var process = Execute(command, args);
+
+        var output = process.StandardOutput.ReadToEnd();
 
         process.WaitForExit();
 
-        Console.WriteLine(process.StandardOutput.ReadToEnd());
+        Console.WriteLine(output);
     }
 
     public abstract Task ExecuteAsync();
-    protected async Task ExecuteAsync(string command)
+    protected async Task ExecuteAsync(string command, string args)
     {
-        using var process = Execute(command);
+        using var process = Execute(command, args);
+
+        var output = await process.StandardOutput.ReadToEndAsync();
 
         await process.WaitForExitAsync();
 
-        Console.WriteLine(process.StandardOutput.ReadToEndAsync());
+        Console.WriteLine(output);
     }
 }
