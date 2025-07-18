bd6c8ee72ee655a283f1932f100bae7c3d37d63a
Arensbak <char@novax.dk>
2025-07-18 11:16:27 +0200

    style: Add spaces after icons for easier formatting.

--- src/Floe.Core/Constants/Diagnostics.cs
+++ src/Floe.Core/Constants/Diagnostics.cs
@@ -2,8 +2,8 @@ namespace Floe.Core.Constants;
 
 public static class Diagnostics
 {
-    public const string ErrorSymbol = "✗";
+    public const string ErrorSymbol = "✗ ";
     public const string WarningSymbol = "⚠ ";
-    public const string SuccessSymbol = "✓";
-    public const string InfoIcon = "i";
+    public const string SuccessSymbol = "✓ ";
+    public const string InfoIcon = "i ";
 }
