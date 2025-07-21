ff05e90ab54668406cf81d506d0c3f6b9cdb3ff0
Arensbak <char@novax.dk>
2025-07-21 14:53:57 +0200

    tests: Add a Fixture class that will be used to share the state between the integration tests.

---
+++ tests/Floe.IntegrationTests/GitRepoFixture.cs
@@ -0,0 +1,26 @@
+using Floe.Cli.IntegrationTests;
+using Floe.Core.Models;
+
+[assembly: AssemblyFixture(typeof(GitRepoFixture))]
+namespace Floe.Cli.IntegrationTests;
+
+public class GitRepoFixture
+{
+    public GitRepoFixture()
+    {
+        var tempRepoPath = Path.Combine(AppContext.BaseDirectory, $"tmp-{Guid.NewGuid()}");
+        Directory.CreateDirectory(tempRepoPath);
+        Directory.SetCurrentDirectory(tempRepoPath);
+
+        Git.Init()
+            .ExecuteAndFinish();
+
+        Git.Commit()
+            .WithMessage("initial commit")
+            .AllowEmpty()
+            .ExecuteAndFinish();
+
+        Git.Tag("1.0.0", "1.1.0", "1.1.0-alpha.1", "1.1.0-alpha.23", "1.1.1", "1.1.2", "1.1.3",
+                "1.1.4", "1.2.0", "1.2.1", "1.2.2", "1.2.3", "1.3.0", "1.3.1", "1.4.0-alpha.0");
+    }
+}
