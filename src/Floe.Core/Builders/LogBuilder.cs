49b01e6d76b874e8b8f2cb91af1188614fe176fc
Arensbak <char@novax.dk>
2025-07-22 14:23:26 +0200

    feat: Add Git.Log() builder.

--- src/Floe.Core/Builders/LogBuilder.cs
+++ src/Floe.Core/Builders/LogBuilder.cs
@@ -7,7 +7,10 @@ public class LogBuilder : GitProcess
 {
 	public LogBuilder() { }
 
-	protected override GitProcess AddArgument(string arg)
+	public LogBuilder RevParse(string branchName) => AddArgument($"{Git.LogFlags.RevParse} {branchName}");
+	public LogBuilder MergeBase(string branchOne, string branchTwo) => AddArgument($"{Git.LogFlags.MergeBase} {branchOne} {branchTwo}");
+
+	protected override LogBuilder AddArgument(string arg)
 	{
 		ArgsBuilder.AppendArgument(arg);
 
@@ -16,4 +19,28 @@ public class LogBuilder : GitProcess
 
 	public override ProcessResult Execute() => base.Execute(Git.BaseName, ArgsBuilder.Build());
 	public override Task<ProcessResult> ExecuteAsync() => base.ExecuteAsync(Git.BaseName, ArgsBuilder.Build());
+
+	public string GetBranchOffCommit(string branchOne, string branchTwo)
+	{
+		var result = Git.Log()
+			.MergeBase(branchOne, branchTwo)
+			.Execute();
+
+		if (!result.WasSuccess)
+			throw new NotImplementedException();
+
+		return result.FirstLine;
+	}
+
+	public string GetLatestCommitOnBranch(string branchName)
+	{
+		var result = Git.Log()
+			.RevParse(branchName)
+			.Execute();
+
+		if (!result.WasSuccess)
+			throw new NotImplementedException();
+
+		return result.FirstLine;
+	}
 }
